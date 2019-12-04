aece4b03ae3fdbcd3c2fcd5201d71d9ff8a7f638
Ahmad Hasrul bin Hussin <hasrulhussin_tk@yahoo.com>
2019-12-05 02:26:47 +0800

    Add files via upload

---
+++ json_sero_test.php
@@ -0,0 +1,20 @@
+
+ [
+            {
+                "name": "MEowsalot",
+                "species": "cat",
+                "foods": {
+                    "likes": ["tuna", "catnip"],
+                    "dislikes": ["ham", "zuchini"]
+                }
+            },
+            {
+                "name": "Barky",
+                "species": "dog",
+                "foods": {
+                    "likes": ["bones", "carrots"],
+                    "dislikes": ["tuna"]
+                }
+            }
+
+]
